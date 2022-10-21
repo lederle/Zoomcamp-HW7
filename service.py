81eab0d260d9c2102d2491d248a16a913c30592f
mike lederle <mrlederle@gmail.com>
2022-10-21 00:30:21 -0400

    add logic for decision

--- service.py
+++ service.py
@@ -10,5 +10,14 @@ service = bentoml.Service("credit_risk_classifier", runners = [model_runner])
 
 @service.api(input = JSON(), output = JSON())
 def classify(application_data):
-    prediction = model_runner.predict.run(application_data)
-    return { "status" : "APPROVED" }
+    client = dv.transform(application_data)
+    prediction = model_runner.predict.run(client)
+    print(prediction)
+
+    result = prediction[0]
+    if result > 0.5:
+        return { "status": "DENIED" }
+    elif result > 0.23:
+        return { "status": "MAYBE" }
+    else:
+        return { "status" : "APPROVED" }
