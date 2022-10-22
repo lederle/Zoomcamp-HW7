6c216c1bdb71cf58f085629b30bb5daca2613172
mike lederle <mrlederle@gmail.com>
2022-10-21 21:17:08 -0400

    output for Q3

--- hw7.py
+++ hw7.py
@@ -2,3 +2,5 @@ import os
 
 print(f"1) bentoml version: {os.popen('bentoml --version').read().rstrip()}")
 print("2) size is 197kb, so best answer is 114kb")
+
+print("3) class UserProfile(BaseModel):\n\tname: str\n\tage: int\n\tcountry: str\n\trating: float\n")
