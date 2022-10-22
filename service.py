f1fff258872c299afbcab2527a03e1658e46457f
mike lederle <mrlederle@gmail.com>
2022-10-21 21:00:55 -0400

    pydantic model for data validation

--- service.py
+++ service.py
@@ -1,6 +1,24 @@
 import bentoml
 from bentoml.io import JSON
 
+from pydantic import BaseModel
+
+class CreditApplication(BaseModel):
+    seniority: int
+    home: str
+    time: int
+    age: int
+    marital: str
+    records: str
+    job: str
+    expenses: int
+    income: float
+    assets: float
+    debt: float
+    amount: int
+    price: int
+
+
 model_reference = bentoml.xgboost.get("credit_risk_model:latest")
 dv = model_reference.custom_objects["dictVectorizer"]
 model_runner = model_reference.to_runner()
@@ -8,9 +26,11 @@ model_runner = model_reference.to_runner()
 service = bentoml.Service("credit_risk_classifier", runners = [model_runner])
 
 
-@service.api(input = JSON(), output = JSON())
-def classify(application_data):
+@service.api(input = JSON(pydantic_model = CreditApplication), output = JSON())
+def classify(credit_application):
+    application_data = credit_application.dict()
     client = dv.transform(application_data)
+    # TODO: naming is wack
     prediction = model_runner.predict.run(client)
     print(prediction)
 
