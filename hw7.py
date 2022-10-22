cf947da6567bbfae2a670fb18c5505c24c54ec67
mike lederle <mrlederle@gmail.com>
2022-10-21 23:52:55 -0400

    Q4 output

--- hw7.py
+++ hw7.py
@@ -4,3 +4,5 @@ print(f"1) bentoml version: {os.popen('bentoml --version').read().rstrip()}")
 print("2) size is 197kb, so best answer is 114kb")
 
 print("3) class UserProfile(BaseModel):\n\tname: str\n\tage: int\n\tcountry: str\n\trating: float\n")
+
+print(f"4) scikit version in coolmodel: {os.popen('bentoml models get mlzoomcamp_homework:qtzdz3slg6mwwdu5 | grep scikit').read().strip()}")
